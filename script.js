1fabc9e9cb61a75840916de0e188a5565a70d3d5
Okiki Areo <okikiareo@gmail.com>
2023-10-17 23:58:11 +0100

    fqs

---
+++ script.js
@@ -0,0 +1,26 @@
+var btn = document.querySelector('.btn')
+var toggle = document.querySelector(".toggle")
+var p = document.querySelector(".p")
+
+btn.addEventListener("click", function(){
+    if(btn.src = "img/plus-circle.svg"){
+        btn.src = "img/minus-circle.svg";
+        toggle.style.background = "none"
+        p.style.display = "none";
+    }
+    else{
+        btn.src = "img/plus-circle.svg"
+        toggle.style.background = "#EAECF0"
+    }
+})
+
+// var btn = document.querySelector('.btn');
+// var img = btn.previousElementSibling;
+
+// btn.addEventListener("click", function () {
+//     if (img.src.endsWith("plus-circle.svg")) {
+//         img.src = "img/minus-circle.svg";
+//     } else {
+//         img.src = "img/plus-circle.svg";
+//     }
+// });
